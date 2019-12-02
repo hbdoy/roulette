b058b97a2970798e0ab35cb1ec901ca2cc485698
ryan_lee <ryan_lee@gss.com.tw>
2019-12-02 13:06:10 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,292 @@
+var data;
+
+var originData = {
+    "Ryan": {
+        "status": "",
+        "photo": "Ryan.jpg",
+        "data": [
+            {
+                color: '#3f297e',
+                text: '超簡單',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#1d61ac',
+                text: '簡單',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#169ed8',
+                text: '普通',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#209b6c',
+                text: '困難',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#60b236',
+                text: '超困難',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#efe61f',
+                text: '地獄級',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#f7a416',
+                text: '送分題',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#e6471d',
+                text: '隨便猜',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#dc0936',
+                text: '保證會',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#e5177b',
+                text: '牛刀小試',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#be107f',
+                text: '祝你好運',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#881f7e',
+                text: '福利',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            }
+        ]
+    },
+    "Neil": {
+        "status": "",
+        "photo": "Neil.jpg",
+        "data": [
+            {
+                color: '#3f297e',
+                text: '超簡單',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#1d61ac',
+                text: '簡單',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#169ed8',
+                text: '普通',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#209b6c',
+                text: '困難',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#60b236',
+                text: '超困難',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#efe61f',
+                text: '地獄級',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#f7a416',
+                text: '送分題',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#e6471d',
+                text: '隨便猜',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#dc0936',
+                text: '保證會',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#e5177b',
+                text: '牛刀小試',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#be107f',
+                text: '祝你好運',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#881f7e',
+                text: '福利',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            }
+        ]
+    },
+    "Test": {
+        "status": "",
+        "photo": "Test.jpg",
+        "data": [
+            {
+                color: '#3f297e',
+                text: '超簡單',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#1d61ac',
+                text: '簡單',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#169ed8',
+                text: '普通',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#209b6c',
+                text: '困難',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#60b236',
+                text: '超困難',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#efe61f',
+                text: '地獄級',
+                fileType: "image",
+                fileName: "test.jpg",
+                show: true
+            },
+            {
+                color: '#f7a416',
+                text: '送分題',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#e6471d',
+                text: '隨便猜',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#dc0936',
+                text: '保證會',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#e5177b',
+                text: '牛刀小試',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#be107f',
+                text: '祝你好運',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            },
+            {
+                color: '#881f7e',
+                text: '福利',
+                fileType: "audio",
+                fileName: "test.mp3",
+                show: true
+            }
+        ]
+    }
+};
+
+getData();
+
+function getData() {
+    data = JSON.parse(localStorage.getItem('myData'));
+    if (data == null) {
+        resetData();
+    }
+}
+
+function resetData() {
+    data = originData;
+    localStorage.setItem('myData', JSON.stringify(data));
+}
+
+function syncData() {
+    localStorage.setItem('myData', JSON.stringify(data));
+}
---
+++ js/script.js
@@ -0,0 +1,264 @@
+var ans = "";
+
+(function ($) {
+  $.fn.extend({
+
+    roulette: function (options) {
+      var defaults = {
+        angle: 0,
+        // angleOffset: -45,
+        angleOffset: 0,
+        speed: 5000,
+        easing: "easeInOutElastic",
+      };
+
+      var opt = $.extend(defaults, options);
+
+      return this.each(function () {
+        showUsedMedia();
+
+        var o = opt;
+
+        var $wrap = $(this);
+        var $btnStart = $wrap.find("#btn-start");
+        var $roulette = $wrap.find(".roulette");
+        var wrapW = $wrap.width();
+        var angle = o.angle;
+        var angleOffset = o.angleOffset;
+        var speed = o.speed;
+        var esing = o.easing;
+        var itemSize = data[ans].data.length;
+        var itemSelector = "item";
+        var labelSelector = "label";
+        var d = 360 / itemSize;
+        var borderTopWidth = wrapW;
+        var borderRightWidth = tanDeg(d);
+
+        for (i = 1; i <= itemSize; i += 1) {
+          var idx = i - 1;
+          var rt = i * d + angleOffset;
+          var itemHTML = $('<div class="' + itemSelector + '">');
+          var labelHTML = '';
+          labelHTML += '<p class="' + labelSelector + '">';
+          labelHTML += '	<span class="text">' + data[ans].data[idx].text + '<\/span>';
+          labelHTML += '<\/p>';
+
+          $roulette.append(itemHTML);
+          $roulette.children("." + itemSelector).eq(idx).append(labelHTML);
+          $roulette.children("." + itemSelector).eq(idx).css({
+            "left": wrapW / 2,
+            "top": -wrapW / 2,
+            "border-top-width": borderTopWidth,
+            "border-right-width": borderRightWidth,
+            "border-top-color": data[ans].data[idx].show ? data[ans].data[idx].color : "black",
+            "transform": "rotate(" + rt + "deg)"
+          });
+
+          var textH = parseInt(((2 * Math.PI * wrapW) / d) * .5);
+
+          $roulette.children("." + itemSelector).eq(idx).children("." + labelSelector).css({
+            "height": textH + 'px',
+            "line-height": textH + 'px',
+            "transform": 'translateX(' + (textH * 1.3) + 'px) translateY(' + (wrapW * -.3) + 'px) rotateZ(' + (90 + d * .5) + 'deg)',
+            "color": data[ans].data[idx].show ? "white" : "grey"
+          });
+
+        }
+
+        function tanDeg(deg) {
+          var rad = deg * Math.PI / 180;
+          return wrapW / (1 / Math.tan(rad));
+        }
+
+
+        $btnStart.on("click", function () {
+          rotation();
+        });
+
+        function rotation() {
+          var completeA, actualA, index;
+          do {
+            if (!chkNotEmpty()) {
+              alert("所有選項已開出");
+              return;
+            }
+            completeA = ra();
+            actualA = completeA % 360 / d;
+            index = (parseInt(itemSize - actualA) - 1) == -1 ? itemSize - 1 : parseInt(itemSize - actualA) - 1;
+            // console.log(completeA, actualA, index);
+          } while (!data[ans].data[index].show);
+
+          // console.log(completeA);
+
+          $roulette.rotate({
+            angle: angle,
+            animateTo: completeA,
+            center: ["50%", "50%"],
+            easing: $.easing.esing,
+            callback: function () {
+              // var currentA = $(this).getRotateAngle();
+              $roulette.children("." + itemSelector).eq(index).css({
+                "border-top-color": 'black'
+              });
+              $roulette.children("." + itemSelector).eq(index).children("." + labelSelector).css({
+                "color": "grey"
+              });
+              data[ans].data[index].show = false;
+              syncData();
+              showUsedMedia();
+              createQueModalContent(index);
+              // alert("pop up: , " + data[ans].data[index].text);
+            },
+            duration: speed
+          });
+        }
+
+        function r(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        // 產生旋轉角度
+        function ra() {
+          var completeA = 360 * r(5, 10) + r(0, 360);
+          // 若在線上則偏移
+          if (completeA % 360 % d == 0) {
+            completeA += 10;
+          }
+          return completeA;
+        }
+
+        // 確認是否還有可用抽取項目
+        function chkNotEmpty() {
+          return data[ans].data.filter(item => item.show == true).length > 0;
+        }
+
+        // 列出已解鎖素材
+        function showUsedMedia() {
+          let contentImg = "";
+          let contentAudio = "";
+          for (let item of data[ans].data) {
+            if (!item.show) {
+              if (item.fileType == "image") {
+                contentImg += `<div class='col-3'><img class='img-fluid' src='./media/${ans}/${item.fileName}'></div>`;
+              } else if (item.fileType == "audio") {
+                contentAudio += `<div class='col-4'><audio controls><source src="./media/${ans}/${item.fileName}" type="audio/mpeg"></audio></div>`;
+              }
+            }
+          }
+          $("#shownMedia .card-body .imgMedia").html(contentImg);
+          $("#shownMedia .card-body .voiceMedia").html(contentAudio);
+        }
+
+        // 顯示新抽出之提示素材
+        function createQueModalContent(index) {
+          let content = "";
+          if (data[ans].data[index].fileType == "image") {
+            content = `<img class='img-fluid' src='./media/${ans}/${data[ans].data[index].fileName}'>`;
+          } else if (data[ans].data[index].fileType == "audio") {
+            content = `<audio controls><source src="./media/${ans}/${data[ans].data[index].fileName}" type="audio/mpeg"></audio>`;
+          }
+          $("#showGuessElement .modal-body").html(content);
+          $("#showGuessElement").modal();
+        }
+      });
+    }
+  });
+})(jQuery);
+
+$(function () {
+  $('#allQuestion').hide();
+  chooseQuestionStatus();
+  createQuestion();
+  eventBind();
+
+  function createQuestion() {
+    var content = "";
+    for (let item in data) {
+      content += `<div class='col-md-4 col-6'>
+          <div class='question ${item} my-2' data-ans='${item}' data-status='${data[item].status}'>
+            <img class='img-fluid' width='150px' src='${data[item].status == "success" ? "./img/" + data[item].photo : "./img/people.png"}'>
+          </div>
+        </div>`;
+    }
+    $("#allQuestion .row").html(content);
+    $(".question").each(function () {
+      // console.log($(this).data("status"));
+      if ($(this).data("status") == "success") {
+        $(this).css({
+          backgroundColor: "green",
+          color: "white"
+        });
+      } else if ($(this).data("status") == "fail") {
+        $(this).css({
+          backgroundColor: "grey",
+          color: "white"
+        });
+      }
+    })
+    $('#allQuestion').show();
+  }
+
+  function chooseQuestionStatus() {
+    ans = "";
+    $('#rouPage').hide();
+    $('#allQuestion').show();
+    $('#indexResetBtn').show();
+  }
+
+  function ansQuestionStatus() {
+    $('#rouPage').show();
+    $('#allQuestion').hide();
+    $('#indexResetBtn').hide();
+  }
+
+  function eventBind() {
+    $('.resetBtn').click(function () {
+      if (confirm("確定要重製資料嗎?")) {
+        resetData();
+        location.reload();
+      }
+    })
+
+    $("#allQuestion").on("click", ".question", function () {
+      // console.log(this.dataset.ans);
+      ans = this.dataset.ans;
+      $('.box-roulette').roulette();
+      ansQuestionStatus();
+    })
+
+    $("#goIndex").click(function () {
+      chooseQuestionStatus();
+    })
+
+    $("#right").click(function () {
+      if (confirm("繼續?")) {
+        // $(`.${ans}`).removeClass("question");
+        $(`.${ans}`).css({
+          backgroundColor: "green"
+        });
+        $(`.${ans} img`).attr("src", `./img/${data[ans].photo}`);
+        data[ans].status = "success";
+        syncData();
+      }
+    })
+
+    $("#wrong").click(function () {
+      if (confirm("繼續?")) {
+        $(`.${ans}`).css({
+          backgroundColor: "red"
+        });
+        data[ans].status = "fail";
+        syncData();
+      }
+    })
+
+    $("#seeAns").click(function () {
+      alert(ans);
+    })
+
+    // 若是音檔則避免沒有暫停就關掉 modal 導致背景持續撥放
+    $('#showGuessElement').on('hidden.bs.modal', function (e) {
+      $("#showGuessElement .modal-body").html("");
+    })
+  }
+});
